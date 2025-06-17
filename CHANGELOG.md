8db74391de4a1aefc05576bc18197894f589fa4d
Will Yelton <willhyelton@gmail.com>
2025-06-17 18:05:04 -0400

    Updates to 1.21.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,3 @@
-# Pillager Deterrent Changelog
-## 1.0.1
-### Fixes
-- Fixes an issue where vanilla white banners would drop Pillager Warding Banners
-- Fixes an issue where wall banners didn't work (may have to break and replace them)
-- Fixes an issue where banners didn't work in some cases
-
+# Pillager Deterrent 1.21.6 Changelog
 ## 1.0.0
-### Additions
-- Adds the pillager warding ring and the pillager warding banner
+- Updates to 1.21.6
--- src/main/java/dev/willyelton/pillagerdeterrent/datagen/PillagerDeterrentItemTags.java
+++ src/main/java/dev/willyelton/pillagerdeterrent/datagen/PillagerDeterrentItemTags.java
@@ -4,19 +4,22 @@ import dev.willyelton.pillagerdeterrent.PillagerDeterrent;
 import dev.willyelton.pillagerdeterrent.Registration;
 import dev.willyelton.pillagerdeterrent.tag.PillagerDeterrentTags;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.data.tags.TagsProvider;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 import java.util.concurrent.CompletableFuture;
 
-public class PillagerDeterrentItemTags extends ItemTagsProvider {
+public class PillagerDeterrentItemTags extends TagsProvider<Item> {
     public PillagerDeterrentItemTags(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, CompletableFuture<TagLookup<Block>> blockTags) {
-        super(output, lookupProvider, blockTags, PillagerDeterrent.MODID);
+        super(output, Registries.ITEM, lookupProvider, PillagerDeterrent.MODID);
     }
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-        tag(PillagerDeterrentTags.PILLAGER_WARD).add(Registration.PILLAGER_RING.get());
+        this.getOrCreateRawBuilder(PillagerDeterrentTags.PILLAGER_WARD)
+            .addElement(Registration.PILLAGER_RING.getId());
     }
 }
