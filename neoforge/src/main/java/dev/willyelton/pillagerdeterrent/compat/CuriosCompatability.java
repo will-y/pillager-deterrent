e59e2917ba41b14cd84846da9bf8e5dcb53064b7
Will Yelton <willhyelton@gmail.com>
2026-04-02 21:03:59 -0400

    Failed attempt to do multiloader

---
+++ neoforge/src/main/java/dev/willyelton/pillagerdeterrent/compat/CuriosCompatability.java
@@ -0,0 +1,11 @@
+package dev.willyelton.pillagerdeterrent.compat;
+
+public class CuriosCompatability {
+//    public static Optional<ItemStack> getCuriosItems(Player player, Predicate<ItemStack> filter) {
+//        if (!ModList.get().isLoaded("curios")) {
+//            return Optional.empty();
+//        }
+//
+//        return CuriosApi.getCuriosInventory(player).flatMap(iCuriosItemHandler -> iCuriosItemHandler.findFirstCurio(filter)).map(SlotResult::stack);
+//    }
+}
